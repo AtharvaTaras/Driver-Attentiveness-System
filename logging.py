110f157b5ccfdcbd8d6e2115602d6c524f2af592
AtharvaTaras <atharvataras26@gmail.com>
2023-01-11 21:40:38 +0530

    Updated logging utility

--- logging.py
+++ logging.py
@@ -4,10 +4,28 @@ import os
 if 'logs' not in os.listdir():
     os.mkdir('logs')
 
-filename = ('logs/ ' + (date.today().strftime("%d/%m/%Y").replace('/', '-')) + '.txt')
-current_time = datetime.now().strftime("%H:%M:%S")
 
-message = 'Test Log'
+def text_log(message: str, curr_time=False,  show_console=False, filename=False):
 
-with open(filename, 'a') as f:
-    f.write(f'{current_time} --- {message} \n')
+    if not filename:
+        filename = ('logs/Log ' + (date.today().strftime("%d/%m/%Y").replace('/', '-')) + '.txt')
+
+    if not curr_time:
+        curr_time = datetime.now().strftime("%H:%M:%S")
+
+    with open(filename, 'a') as f:
+        f.write(f'{curr_time} - {message} \n')
+
+        if show_console:
+            print(f'Saved log message {message} in {filename} at {curr_time} successfully.')
+
+
+def clear_logs(location=False):
+
+    if not location:
+        os.chdir('logs/')
+
+        for file in os.listdir():
+            if file.endswith('.txt'):
+                os.remove(file)
+                print(f'Deleted file {file}')
