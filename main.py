bec0d58e170cc8776a181929379bce513107ebe8
AtharvaTaras <atharvataras26@gmail.com>
2023-01-19 02:35:19 +0530

    Updated

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ print('Imported libraries successfully.')
 
 # Global Constants
 
-HAAR_DATA = 'data/frontfacedata.xml'
+HAAR_DATA = cv2.CascadeClassifier('data/frontfacedata.xml')
 DEBUG = False
 PAD_SIZE = 10
+VID = cv2.VideoCapture(0)
 
 
 def find_face() -> None:
