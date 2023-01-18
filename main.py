e6677e8696b791db42611160dcbbac3e67513071
AtharvaTaras <atharvataras26@gmail.com>
2023-01-19 02:29:38 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+from datetime import date, datetime
+
+import numpy as np
+
+from logging import *
+print('Imported libraries successfully.')
+
+# Global Constants
+
+HAAR_DATA = 'data/frontfacedata.xml'
+DEBUG = False
+PAD_SIZE = 10
+
+
+def find_face() -> None:
+    pass
+
