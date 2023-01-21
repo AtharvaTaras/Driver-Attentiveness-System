80c104092e3a3ab37d06d80e264f910a2f82ba93
AtharvaTaras <atharvataras26@gmail.com>
2023-01-21 20:18:37 +0530

    updated

--- face_landmarks.py
+++ face_landmarks.py
@@ -30,22 +30,23 @@ while True:
     if ret:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = detector(gray)
+        frame = cv2.resize(frame, dsize=(frame.shape[1] * 2, frame.shape[0] * 2))
 
         for face in faces:
             landmarks = predictor(gray, face)
 
             for n in range(0, 68):
-                x = landmarks.part(n).x
-                y = landmarks.part(n).y
-                cv2.circle(frame, (x, y), 2, (255, 255, 255), -1)
+                x = (landmarks.part(n).x) * 2
+                y = (landmarks.part(n).y) * 2
+                #cv2.circle(frame, (x, y), 4, (150, 150, 150), 1)
+
                 cv2.putText(img=frame,
                             text=str(n),
                             org=(x, y),
                             fontFace=cv2.FONT_HERSHEY_PLAIN,
-                            fontScale=1,
+                            fontScale=0.65,
                             thickness=1,
                             color=(255, 255, 255))
 
-            frame = cv2.resize(frame, dsize=(frame.shape[1] * 2, frame.shape[0] * 2))
             cv2.imshow("Facial Landmarks", frame)
             cv2.waitKey(1)
